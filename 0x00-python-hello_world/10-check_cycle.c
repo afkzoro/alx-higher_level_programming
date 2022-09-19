d7f5f2853f4e4644f408ed0a11d42d384dc4c69b
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-19 12:05:08 +0100

    commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks for a loop in a linked list
+ *
+ * @list: Pointer to list
+ * Return: 1 if there's a linked list 0 if there's none
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
