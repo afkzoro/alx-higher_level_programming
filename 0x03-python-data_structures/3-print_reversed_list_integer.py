41bedbad7a1898cdcca3a0c98e65964b1016d206
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-25 12:14:32 +0100

    commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    else:
+    elif my_list:
         for i in reversed(my_list):
             print('{:d}'.format(i))
