a8c91507ac339d6e6cc26c1d242abe3a3aa2212d
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-25 11:13:58 +0100

    commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     n_list = my_list.copy()
     if idx < 0:
         return n_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return n_list
     else:
         n_list[idx] = element
