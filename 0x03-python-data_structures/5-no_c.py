224fad7eed868cdac6a1d38e0e353c1e0b22cac4
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-25 12:57:29 +0100

    commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        if i != 67 and i != 99:
-            print('{:d}'.format(my_string[i]))
+    string = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(string))
