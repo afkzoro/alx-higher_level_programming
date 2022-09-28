0cd62efdf9a8eeb13b2f8b9e82a4dcf578f9c5b5
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-28 20:31:33 +0100

    commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    n_list = my_list.copy()
-    for i, item in enumerate(n_list):
-        if item == search:
-            n_list[i] = replace
-            return n_list
+    return [replace if i == search else i for i in my_list]
