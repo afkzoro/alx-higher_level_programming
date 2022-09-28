130b1e83c030eeaaa206e9f7954e539be96e2105
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-28 21:01:33 +0100

    commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_list = a_dictionary.copy()
-
-    for i, j in new_list.items():
-        new_list[j] = i * 2
-
-    return new_list
+    """Return a new dictionary with all values multipled by 2."""
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
