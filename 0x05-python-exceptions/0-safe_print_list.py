5f0166bb4e6386f42fb49206416a4ff376d9ca65
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-10 07:35:40 +0100

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    new_list = []
+    num = ""
     try:
         for i in range(0, x):
-            new_list.append(my_list[i])
+            num += str(my_list[i])
     except IndexError:
-        new = [str(int) for int in new_list]
-        return "".join(new)
-
-    new = [str(int) for int in new_list]
-    return "".join(new)
+        return int(num)
+    return int(num)
