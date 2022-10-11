9003ab2b4cf7af9eeb62b92691b439454b838c93
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-11 12:25:50 +0100

    commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+""" Square Class"""
+
+
 class Square():
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+""" Square Class """
 class Square():
+    """__init__
+
+    The __init__ method .....
+
+    Attributes:
+        size(int): .......
+    """
     def __init__(self, size):
         self.__size = size
