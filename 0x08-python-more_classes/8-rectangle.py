f429fc6cc8e3a90e93f29c26d3e1e52b72222e43
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-17 12:26:24 +0100

    commit

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
         _type_: _description_
     """
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """_summary_
