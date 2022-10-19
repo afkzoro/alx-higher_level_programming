d3a9f8d6d25edd06f53eec76bf669710e6440980
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-19 17:39:38 +0100

    commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,10 +3,8 @@
 
 
 def add_integer(a, b=98):
-    """ Add integers """
-    if type(a) not in (int, float):
-        raise ValueError("a must be an integer")
-    if type(b) not in (int, float):
-        raise ValueError("b must be an integer")
-
-    return int(a) + int(b)
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
