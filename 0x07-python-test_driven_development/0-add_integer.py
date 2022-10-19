98a61f6d20afb979db2270ea9c57ff21ac597b84
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-19 17:40:24 +0100

    commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,19 @@
 
 
 def add_integer(a, b=98):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (int, optional): _description_. Defaults to 98.
+
+    Raises:
+        TypeError: _description_
+        TypeError: _description_
+
+    Returns:
+        _type_: _description_
+    """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
