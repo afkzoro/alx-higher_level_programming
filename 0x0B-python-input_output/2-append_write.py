43f20debff8ccaf3d844fcea0f608bc82346b38f
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-25 12:19:09 +0100

    commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" append_write function """
+
+
 def append_write(filename="", text=""):
+    """ appends text to a file """
     with open(filename, encoding='utf-8') as fp:
         return fp.write(text)
