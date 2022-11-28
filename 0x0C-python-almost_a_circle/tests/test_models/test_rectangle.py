be8eeab1ff6f354986eed9f9992971f401b34550
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-11-28 21:15:24 +0100

    commit

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# test_rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines unittests for models/rectangle.py.
 
 Unittest classes:
@@ -803,6 +801,7 @@ class TestRectangle_to_dictionary(unittest.TestCase):
         r = Rectangle(10, 2, 4, 1, 2)
         with self.assertRaises(TypeError):
             r.to_dictionary(1)
+
 
 if __name__ == "__main__":
     unittest.main()
